d79926032e7709292679a06f75f6873923107ebe
Egor Vdovin <vdovin.mail.ru@mail.ru>
2021-12-10 16:44:30 +0100

    Uploading files to GitHub

---
+++ script.js
@@ -0,0 +1,12 @@
+let mySong = document.getElementById("mySong");
+let icon = document.getElementById("icon");
+icon.onclick = function(){
+    if(mySong.paused){
+        mySong.play();
+        icon.src = "/images/pause.png";
+    }
+    else {
+        mySong.pause();
+        icon.src = "/images/play.png";
+    }
+}
