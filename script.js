0182bda787ca57393d9c245cf6eba564b9f9e341
Egor Vdovin <vdovin.mail.ru@mail.ru>
2021-12-14 21:38:31 +0100

    Added footer and a bit changes in JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let mySong = document.getElementById("mySong");
 let icon = document.getElementById("icon");
-icon.onclick = function(){
-    if(mySong.paused){
+icon.onclick = function() {
+    if(mySong.paused) {
         mySong.play();
         icon.src = "images/pause.png";
     }
